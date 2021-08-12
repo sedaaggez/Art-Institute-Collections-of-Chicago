8fe2b6c5163a33b4aab2318a01cc70eea196c4e2
sedaaggez <seda-aggez@hotmail.com>
2021-08-12 06:42:14 +0300

    create ArtworkAPI.kt and ArtworkAPIService.kt

--- app/src/main/java/com/sedaaggez/chicagoartinstitutecollections/viewmodel/ArtworksViewModel.kt
+++ app/src/main/java/com/sedaaggez/chicagoartinstitutecollections/viewmodel/ArtworksViewModel.kt
@@ -1,10 +1,11 @@
 package com.sedaaggez.chicagoartinstitutecollections.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.sedaaggez.chicagoartinstitutecollections.model.Artwork
 
-class ArtworksViewModel : ViewModel(){
+class ArtworksViewModel(application: Application) : BaseViewModel(application){
 
     val artworks = MutableLiveData<List<Artwork>>()
     val artworkError = MutableLiveData<Boolean>()
